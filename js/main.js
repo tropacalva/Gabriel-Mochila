1a0219add62925cd3e195892fb08ba2828aac6f8
gabriel <134974960+tropacalva@users.noreply.github.com>
2023-11-13 19:18:52 -0300

    s

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("novoiten")
-const form.addEventListener("submit", () => {
+const form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
     console.log("funcionou")
 }
