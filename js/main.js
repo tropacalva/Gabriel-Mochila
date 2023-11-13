727acc4571578ec1e73e2e7fc46d58ed46734fad
gabriel <134974960+tropacalva@users.noreply.github.com>
2023-11-13 19:36:13 -0300

    s

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ const form = document.getElementById("novoIten")
 
 const form.addEventListener("submit", () => {
     console.log("funcionou")
+    const form.addEventListener("submit", (evento) => {
+        evento.preventDefault()
+        console.log("funcionou")  
 })
